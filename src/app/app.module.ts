1eb2ce983f9dcf2febb79ee90e29bb10f73e7ce8
Ibukun Folorunso <ibukun.folorunso@accenture.com>
2020-04-09 21:23:35 +0100

    ibukun commit changes to app module and package json for video player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './screens/dashboard/dashboard.component';
-import { EventsComponent } from './screens/events/events.component';
-import { SideNavComponent } from './components/side-nav/side-nav.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule, MatCardModule } from  '@angular/material';
-import { ChampionshipComponent } from './screens/championship/championship.component';
-import { RaceResultsComponent } from './screens/race-results/race-results.component';
-import { MatInputModule } from '@angular/material/input';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { CarsAndSetupComponent } from './screens/cars-and-setup/cars-and-setup.component';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { DashboardComponent } from "./screens/dashboard/dashboard.component";
+import { EventsComponent } from "./screens/events/events.component";
+import { SideNavComponent } from "./components/side-nav/side-nav.component";
+import {
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+  MatButtonModule,
+  MatCardModule,
+} from "@angular/material";
+import { ChampionshipComponent } from "./screens/championship/championship.component";
+import { RaceResultsComponent } from "./screens/race-results/race-results.component";
+import { MatInputModule } from "@angular/material/input";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { CarsAndSetupComponent } from "./screens/cars-and-setup/cars-and-setup.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatVideoModule } from "mat-video";
 
 @NgModule({
   declarations: [
@@ -22,7 +30,7 @@ import { CarsAndSetupComponent } from './screens/cars-and-setup/cars-and-setup.c
     SideNavComponent,
     ChampionshipComponent,
     RaceResultsComponent,
-    CarsAndSetupComponent
+    CarsAndSetupComponent,
   ],
   imports: [
     BrowserModule,
@@ -35,9 +43,10 @@ import { CarsAndSetupComponent } from './screens/cars-and-setup/cars-and-setup.c
     MatIconModule,
     MatInputModule,
     FlexLayoutModule,
-    MatCardModule
+    MatCardModule,
+    MatVideoModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
