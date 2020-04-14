d7fec43b70b834cb165613393c394333c4699b65
Armstrong <nichola.armstrong@accenture.com>
2020-04-14 18:51:33 +0100

    package.json commit v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppRoutingModule } from "./app-routing.module";
 import { DashboardComponent } from "./screens/dashboard/dashboard.component";
 import { EventsComponent } from "./screens/events/events.component";
 import { SideNavComponent } from "./components/side-nav/side-nav.component";
-import {
-  MatToolbarModule,
-  MatIconModule,
-  MatSidenavModule,
-  MatListModule,
-  MatButtonModule,
-  MatCardModule,
-} from "@angular/material";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { ChampionshipComponent } from "./screens/championship/championship.component";
 import { RaceResultsComponent } from "./screens/race-results/race-results.component";
 import { MatInputModule } from "@angular/material/input";
